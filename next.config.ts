d69289d39b8a6c0bd0dffc8acca5d6ea1e8d8298
zazaza54321 <zazaza54321@gmail.com>
2025-10-14 14:21:16 +0000

    actualizacion paquete next

--- next.config.ts
+++ next.config.ts
@@ -1,50 +1,6 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            // Excluir `express` del paquete del servidor, ya que causa problemas con Genkit.
-            config.externals.push('express');
-        }
-        return config;
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://localhost:5000/api/:path*', // Redirige al backend en el puerto 5000
-            },
-        ]
-    },
-    /* config options here */
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    serverExternalPackages: ["@genkit-ai/core"],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
